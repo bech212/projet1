41b03326d00d0e4cdb47666c2735cff01c9950a2
BrandonNgassa16 <110480877+BrandonNgassa16@users.noreply.github.com>
2022-08-10 15:33:13 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -29,7 +29,7 @@ Route::get('/', function () {return view('welcome');})
 ->name('welcome');
 
 
-Route::get('service', [ServiceContoller::class, 'index'])->name('service');
+
 
 Route::get('contact', [ContactContoller::class, 'index'])->name('contact');
 
